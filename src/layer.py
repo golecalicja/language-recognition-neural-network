2b56a82928dfb9d3b2eaa046fcff416839c7d7d5
golecalicja <golecalicja@gmail.com>
2022-04-11 15:52:49 +0200

    cleaned

--- src/layer.py
+++ src/layer.py
@@ -7,6 +7,5 @@ class Layer:
         language_to_activation_value = {}
         for perceptron in self.perceptrons:
             language_to_activation_value[perceptron.language] = perceptron.calculate_activation(row)
-        print(language_to_activation_value)
         return max(language_to_activation_value, key=language_to_activation_value.get)
 
