ced7156b34281a42816096a566b7ff107ddae9a6
golecalicja <golecalicja@gmail.com>
2022-04-20 20:34:56 +0200

    fixed variable name

--- src/data_combiner.py
+++ src/data_combiner.py
@@ -10,11 +10,11 @@ def combine_texts_to_csv(path):
         if os.path.isdir(path + item):
             languages.append(item)
 
-    list_of_text = []
+    texts = []
     for language in languages:
         for file in os.listdir(os.path.join(path, language)):
             with open(os.path.join(path, language, file), encoding='utf8', mode='r') as f:
                 text = f.read()
-            list_of_text.append((text, language))
-        df = pd.DataFrame(list_of_text, columns=['Text', 'Language'])
+            texts.append((text, language))
+        df = pd.DataFrame(texts, columns=['Text', 'Language'])
         df.to_csv(os.path.join(path, combined_filename))
